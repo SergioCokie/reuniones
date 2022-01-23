154fa0c469890bfc4955e50fcb9d2b65bbc17201
Sergio <16adonaysergio@gmail.com>
2022-01-23 09:27:39 -0600

    probando

--- application/controllers/Procesos/Distritos.php
+++ application/controllers/Procesos/Distritos.php
@@ -10,6 +10,7 @@ include (APPPATH."controllers/Padre.php");
         public function index()
         {
             //comentario para ver como se hacen los cambios
+            // de nuevo
             $data["distritos_resumen"] = $this->Procesos_model->getDistritos();
             $data["titulo"] = "Reuniones - Dashboard";
             $this->load->view("Layout/Header", $data);
